8f2f0ed668edbef3290df13b3469bdb1ae926247
Alexander Ryakhin <a.ryahin@trinitydigital.ru>
2022-01-30 10:38:36 +0300

    Initial Commit

---
+++ Haptics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Haptics
+//
+//  Created by Alexander Ryakhin on 1/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Haptics/HapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticsApp.swift
+//  Haptics
+//
+//  Created by Alexander Ryakhin on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct HapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
