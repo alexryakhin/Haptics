a7ceb5a8ffc77839a3f83d0d9ed73bf804f4e43d
Alexander Ryakhin <a.ryahin@trinitydigital.ru>
2022-01-30 10:49:52 +0300

    final commit

--- Haptics/ContentView.swift
+++ Haptics/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(HapticCase.allCases, id: \.self) { haptic in
+                Button {
+                    haptic.vibrate()
+                } label: {
+                    Text("\(haptic.rawValue.capitalized)")
+                }
+            }
+            .navigationTitle("Haptics")
+        }
     }
 }
 
